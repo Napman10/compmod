3c43a25a15c8d5776e7acd278e09b83415e1a80c
Napman10 <ilya.kashapov99@gmail.com>
2020-06-18 11:56:39 +0500

    fixed code

--- code.py
+++ code.py
@@ -3,8 +3,34 @@ from random import random as rndbl
 from math import exp
 import matplotlib.pyplot as plt
 
-inf = '∞'
+inf = 99999
 outputText = ""
+def algorithm(t, k, L, matrix, s):
+    step = 1
+    while t>k:                  
+        step+=1
+        for i in range(L):
+            s1 = swap_in_list(s)
+            delta = f(matrix, s1) - f(matrix, s)
+            if delta<=0:                
+                s = s1     
+            else:
+                r = rndbl()
+                p =exp(-delta/t)
+                if r<p:
+                    s=s1             
+        t*=k
+    return s, step
+
+def find_mid(st, sk, L, matrix, d, n, s):
+    t, k = st, sk
+    out = "Среднее с параметром t = {0}, k = {1} ".format(t, k)
+    ret = 0
+    for st in range(d):
+        s = algorithm(t, k, L, matrix, s)[0]
+        ret+=f(matrix, s)
+    out+=" равно {0}".format(ret/d)
+    prints(out)
 
 def outputFile():
     with open("output.txt", "w", encoding='utf-8') as file:
@@ -75,15 +101,22 @@ def generate_symmetric_matrix(n, auto=True):
 
 def main():
     while True:
-        x = input("Выберите действие:\n1. Ввести матрицу самостоятельно\n2. Сгенерировать автоматически\nпо умолчанию. Выход\nВаш выбор: ")
+        x = input("Выберите действие:\n1. Ввести матрицу самостоятельно\n2. Сгенерировать автоматически\n3. Выбрать из файла\nпо умолчанию. Выход\nВаш выбор: ")
         if x=='1':
-            auto = False
+            n = int(input("Введите размер матрицы: "))
+            matrix = generate_symmetric_matrix(n, False)
         elif x=='2':
-            auto = True
+            n = int(input("Введите размер матрицы: "))
+            matrix = generate_symmetric_matrix(n, True)
+        elif x=='3':
+            num_mat = int(input("Выберите одну из 4 матриц (по умолчанию 1): "))
+            if num_mat not in range(1,5):
+                num_mat = 1
+            with open("data/{0}.txt".format(num_mat), "r") as file:
+                matrix = [list(map(int, row.split())) for row in file.readlines()]
+                n = len(matrix[0])
         else:
             break
-        n = int(input("Введите размер матрицы: "))
-        matrix = generate_symmetric_matrix(n, auto)
         printMat(matrix)
         T = [800,1200,1800]
         K = [0.5,0.6,0.7,0.8,0.9]
@@ -91,34 +124,28 @@ def main():
         s = generate_random_solution(n)
         s0 = list(s)
         colors = ['r','g','b']
+        s_fond = list()
         for t0 in T:
             solves = list()
             for k0 in K:
                 t,k = t0,k0
                 prints("t = {0}, k = {1}".format(t,k))
                 prints("Исходное решение: \ns = {0}, f(s) = {1}".format(s, f(matrix, s)))
-                step = 1
-                while t>k:                  
-                    step+=1
-                    for i in range(L):
-                        s1 = swap_in_list(s)
-                        delta = f(matrix, s1) - f(matrix, s)
-                        if delta<=0:                
-                            s = s1     
-                        else:
-                            r = rndbl()
-                            p =exp(-delta/t)
-                            if r<p:
-                                s=s1             
-                    t*=k
+                s, step = algorithm(t0, k0, L, matrix, s0)
                 prints("Лучшее решение\ns = {0}; f(s) = {1}\nЗавершено на итерации номер {2} при t = {3}\n".format(s, f(matrix,s),step, round(t,3)))
                 solves.append(f(matrix, s))
-                s = s0 
+                s_fond.append(f(matrix, s))
+                if solves[-1]==min(s_fond):
+                    st, sk = t0, k0
+                    min_s = s
+                s = s0
             plt.plot(K,solves, label='T={0}'.format(t0),color=colors.pop())   
         plt.legend()
         plt.xlabel("k", fontsize=14, fontweight="bold")
         plt.ylabel("f(s)", fontsize=14, fontweight="bold")
         outputFile()
+        prints("Минимальный путь {0} длиной {1} достигнут при t = {2}, k = {3}".format(min_s, f(matrix, min_s), st, sk))
+        find_mid(st, sk, L, matrix, 50, n, s0)
         plt.show()
 
 main()
